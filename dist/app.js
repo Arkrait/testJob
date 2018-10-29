167bb37264bd034c881b2f8a2082e1be75756670
Kirill Mironov <arkrait04@gmail.com>
2018-10-29 21:17:04 +0300

    started working on basic API architecture

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,41 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
+const Web3 = require("web3");
+const fs = require("fs");
+const contractJson = require("../ethereum/build/contracts/CarRegistration.json");
+let config = require("./config.json");
+const abi = contractJson.abi;
+const bc = contractJson.bytecode;
 const app = express();
+const web3 = new Web3(Web3.givenProvider || "ws://localhost:7545");
+// deploy only one contract by saving its' address in a config file
+console.log(config["contractAddress"]);
+if (config["contractAddress"] === null ||
+    config["contractAddress"] === undefined) {
+    web3.eth.getAccounts().then(result => {
+        const contract = new web3.eth.Contract(abi);
+        contract
+            .deploy({
+            data: bc,
+            arguments: []
+        })
+            .send({
+            // set this to registrar address in production
+            from: result[0],
+            gas: 4712388,
+            gasPrice: 100000000000
+        })
+            .then(contract => {
+            config.contractAddress = contract.options.address;
+            fs.writeFileSync("./config.json", JSON.stringify(config));
+            console.log(contract.options.address);
+        });
+    });
+}
+app.get("/:address", (req, res) => {
+    let inputtedAddress = "0x" + req.params.address;
+    console.log(inputtedAddress);
+});
+app.listen(8080, "localhost");
 //# sourceMappingURL=app.js.map
