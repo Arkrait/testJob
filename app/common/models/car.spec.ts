0a11e22ac2f81e1731d12a68a8559a5ffcccee47
Kirill Mironov <arkrait04@gmail.com>
2018-11-13 23:00:38 +0300

    fix tests

--- app/common/models/car.spec.ts
+++ app/common/models/car.spec.ts
@@ -4,11 +4,6 @@ import { app } from "../../app";
 
 describe("car model", function() {
   // deploy contract
-  before(() => {
-    request(app).post(
-      "/api/Cars/1/requestRegister/6cbed15c793ce57650b9877cf6fa156fbef513c4e6134f022a85b1ffdd59b2a1"
-    );
-  });
   it('returns error if "0x" are present in the beginning of an address|private key', function(done) {
     request(app)
       .post(
@@ -23,15 +18,19 @@ describe("car model", function() {
       .end(done);
   });
   it("returns correct eth response when given id and private key", function(done) {
-    request(app)
-      .post(
-        "/api/Cars/1/requestRegister/6cbed15c793ce57650b9877cf6fa156fbef513c4e6134f022a85b1ffdd59b2a1"
-      )
-      .expect(200)
-      .expect(res => {
-        res.body.ethResponse.transactionHash !== null ||
-          res.body.ethResponse.transactionHash !== undefined;
-      })
-      .end(done);
+    this.timeout(5000);
+
+    setTimeout(function() {
+      request(app)
+        .post(
+          "/api/Cars/1/requestRegister/6cbed15c793ce57650b9877cf6fa156fbef513c4e6134f022a85b1ffdd59b2a1"
+        )
+        .expect(200)
+        .expect(res => {
+          res.body.ethResponse.transactionHash !== null ||
+            res.body.ethResponse.transactionHash !== undefined;
+        })
+        .end(done);
+    }, 2500);
   });
 });
--- app/common/models/car.ts
+++ app/common/models/car.ts
@@ -1,97 +1,96 @@
+import Contract from "web3/eth/contract";
 import { CarModel } from "./../../Car.model";
 import web3 from "../../web3instance";
 import getContract from "../../getContract";
 import { PersistedModel, CallbackWithResult } from "loopback";
 
 module.exports = function(Car: typeof CarModel) {
+  let contract: Contract;
+  getContract().then(contr => {
+    contract = contr;
+  });
   /*
    * Probably a really hacky way but this is better then getting contract from a file
    * Plus this helps integrating tests properly
    */
-  getContract().then(contract => {
-    /* I am thinking about ways to change this endpoint so that users don't have to paste their private key
-     * in a request but this will probably require the clien to have MetaMask installed for ease of
-     * authentication
-     */
-    Car.requestRegister = function(
-      id: string,
-      privateKey: string,
-      cb: CallbackWithResult<string>
-    ): any {
-      if (privateKey[0] === "0" && privateKey[1] === "x") {
-        cb(
-          null,
-          "Please provide the private key without specifying the hex type."
-        );
-        return;
-      }
-      const account = web3.eth.accounts.privateKeyToAccount("0x" + privateKey);
-      Car.findById(id, function(err, car) {
-        contract.methods
-          .requestRegistration(
-            car.plate,
-            car.brand,
-            car.model,
-            car.ownerCredentials,
-            account.address,
-            car.horsePower
-          )
-          .send({
-            from: account.address,
-            gas: 4712388,
-            gasPrice: 100000000000
-          })
-          .then(res => {
-            cb(null, res);
-          })
-          .catch(err => {
-            cb(err, null);
-          });
-      });
-    };
-
-    Car.registerCar = function(address: string, cb: CallbackWithResult<any>) {
-      let registrarAccount: string;
-      web3.eth.getAccounts().then(result => (registrarAccount = result[0]));
-
-      if (address[0] === "0" && address[1] === "x") {
-        cb(null, "Please provide the address without specifying the hex type.");
-        return;
-      }
-      contract.events.CarRegistered({}, (err, result) => {
-        console.log(result);
-      });
+  Car.requestRegister = function(
+    id: string,
+    privateKey: string,
+    cb: CallbackWithResult<string>
+  ): any {
+    if (privateKey[0] === "0" && privateKey[1] === "x") {
+      cb(
+        null,
+        "Please provide the private key without specifying the hex type."
+      );
+      return;
+    }
+    const account = web3.eth.accounts.privateKeyToAccount("0x" + privateKey);
+    Car.findById(id, function(err, car) {
       contract.methods
-        .registerCar("0x" + address)
+        .requestRegistration(
+          car.plate,
+          car.brand,
+          car.model,
+          car.ownerCredentials,
+          account.address,
+          car.horsePower
+        )
         .send({
-          from: registrarAccount,
+          from: account.address,
           gas: 4712388,
           gasPrice: 100000000000
         })
-        .then(res => cb(null, res))
-        .catch(err => cb(err, null));
-    };
-
-    Car.getCarsFromAddress = function(
-      address: string,
-      index: number,
-      cb: CallbackWithResult<any>
-    ) {
-      if (address[0] === "0" && address[1] === "x") {
-        cb(null, "Please provide the address without specifying the hex type.");
-        return;
-      }
-      const passedAddress: string = "0x" + address;
-      index -= 1;
-      contract.methods
-        // get the registered car by id, for example /cars/*address*/1 returns [0] of array
-        .getCarByIndex(passedAddress, index)
-        // _call_ instead of _send_ because getCarByIndex is a funcrview
-        .call({
-          from: passedAddress
+        .then(res => {
+          cb(null, res);
         })
-        .then(result => cb(null, result))
-        .catch(err => cb(err, null));
-    };
-  });
+        .catch(err => {
+          cb(err, null);
+        });
+    });
+  };
+
+  Car.registerCar = function(address: string, cb: CallbackWithResult<any>) {
+    let registrarAccount: string;
+    web3.eth.getAccounts().then(result => (registrarAccount = result[0]));
+
+    if (address[0] === "0" && address[1] === "x") {
+      cb(null, "Please provide the address without specifying the hex type.");
+      return;
+    }
+    contract.events.CarRegistered({}, (err, result) => {
+      console.log(result);
+    });
+    contract.methods
+      .registerCar("0x" + address)
+      .send({
+        from: registrarAccount,
+        gas: 4712388,
+        gasPrice: 100000000000
+      })
+      .then(res => cb(null, res))
+      .catch(err => cb(err, null));
+  };
+
+  Car.getCarsFromAddress = function(
+    address: string,
+    index: number,
+    cb: CallbackWithResult<any>
+  ) {
+    if (address[0] === "0" && address[1] === "x") {
+      cb(null, "Please provide the address without specifying the hex type.");
+      return;
+    }
+    const passedAddress: string = "0x" + address;
+    index -= 1;
+    contract.methods
+      // get the registered car by id, for example /cars/*address*/1 returns [0] of array
+      .getCarByIndex(passedAddress, index)
+      // _call_ instead of _send_ because getCarByIndex is a funcrview
+      .call({
+        from: passedAddress
+      })
+      .then(result => cb(null, result))
+      .catch(err => cb(err, null));
+  };
 };
